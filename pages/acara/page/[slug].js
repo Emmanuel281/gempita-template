acbf2cac4db7bf4f8387ed61ae4c7fff93a0c43a
Emmanuel281 <47650883+Emmanuel281@users.noreply.github.com>
2024-01-03 00:09:07 +0700

    Update [slug].js

--- pages/acara/page/[slug].js
+++ pages/acara/page/[slug].js
@@ -1,21 +1,54 @@
+import React, { useState, useEffect } from 'react'
 import Pagination from "@components/Pagination";
 import config from "@config/config.json";
-import cobapagacara from "@config/cobapagacara.json";
 import Base from "@layouts/Baseof";
 import { getListPage, getSingleData } from "@lib/contentParser";
 import { parseMDX } from "@lib/utils/mdxParser";
 import { markdownify } from "@lib/utils/textConverter";
 import Posts from "@partials/Posts";
-const { blog_folder } = config.settingsacara;
+import { useRouter } from 'next/router'
+const { blog_folder, pagination } = config.settingsacara;
 export const revalidate = 10;
 // blog pagination
-const BlogPagination = ({ postIndex, posts, currentPage, pagination }) => {
-  const { frontmatter, content, contentapi } = postIndex;
-  const totalPages = Math.ceil(contentapi.data.length / pagination);
-  const { title } = frontmatter;
+const BlogPagination = () => {
+  const router = useRouter();
+  const [contentapi, setContentapi] = useState({})
+  const [totalPages, setTotalPages] = useState(1)
+  const currentPage = parseInt((router.query && router.query.slug) || 1);
+  let start = 1;
+  if (currentPage > 1) {
+    start = (currentPage - 1) * pagination;
+  }
+  console.log(router.query);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://adm.gempitamilenial.org/service/event-public?start=${start}&count=${pagination}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/cbor",
+          "responsetype": "arraybuffer",
+        }})
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const result = await response.arrayBuffer()
+      const decoded = await cbor.decode(result)
+      setContentapi(decode)
+      console.warn(decoded);
+      setTotalPages(Math.ceil(contentapi.data.length / pagination))
+    }
+ 
+    fetchData().catch((e) => {
+      // handle the error as needed
+      console.error('An error occurred while fetching the data: ', e)
+    })
+  }, [])
+
+  console.log("contentapi");
+  console.log(Object.keys(contentapi).length);
   return (
-    <Base title={title}>
+    <Base title={"Acara Terbaru"}>
       <section className="section">
         <div className="container">
           {markdownify(
@@ -23,7 +56,9 @@ const BlogPagination = ({ postIndex, posts, currentPage, pagination }) => {
             "h1",
             "h1 text-center font-normal text-[56px]"
           )}
-          <Posts
+          {Object.keys(contentapi).length != 0 ? (
+            <>
+            <Posts
             posts={contentapi.data}
             currentPage={currentPage}
             type="acara"
@@ -33,6 +68,9 @@ const BlogPagination = ({ postIndex, posts, currentPage, pagination }) => {
             totalPages={totalPages}
             currentPage={currentPage}
           />
+          </>
+          ) : ("Loading...")}
+          
         </div>
       </section>
     </Base>
@@ -41,53 +79,51 @@ const BlogPagination = ({ postIndex, posts, currentPage, pagination }) => {
 
 export default BlogPagination;
 
-// get blog pagination slug
-export const getStaticPaths = async () => {
-  const getAllSlug = await getSingleData(
-    `http://adm.gempitamilenial.org/service/event-public?start=1&count=1`
-  );
-  const { pagination } = config.settingsacara;
-  const totalPages = Math.ceil(getAllSlug.total_count / pagination);
-  let paths = [];
+// // get blog pagination slug
+// export const getStaticPaths = async () => {
+//   const getAllSlug = await getSingleData(
+//     `http://adm.gempitamilenial.org/service/event-public?start=1&count=1`
+//   );
+//   const { pagination } = config.settingsacara;
+//   const totalPages = Math.ceil(getAllSlug.total_count / pagination);
+//   let paths = [];
 
-  for (let i = 1; i < totalPages; i++) {
-    paths.push({
-      params: {
-        slug: (i + 1).toString(),
-      },
-    });
-  }
-  // console.log("paths");
-  // console.log(paths);
+//   for (let i = 1; i < totalPages; i++) {
+//     paths.push({
+//       params: {
+//         slug: (i + 1).toString(),
+//       },
+//     });
+//   }
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-// get blog pagination content
-export const getStaticProps = async ({ params }) => {
-  const currentPage = parseInt((params && params.slug) || 1);
-  const { pagination } = config.settingsacara;
-  let start = 1;
-  if (currentPage > 1) {
-    start = (currentPage - 1) * pagination;
-  }
-  let postIndex = await getListPage(
-    `content/${blog_folder}/_index.md`,
-    `http://adm.gempitamilenial.org/service/event-public?start=${start}&count=${pagination}`
-  );
+// // get blog pagination content
+// export const getStaticProps = async ({ params }) => {
+//   const currentPage = parseInt((params && params.slug) || 1);
+//   const { pagination } = config.settingsacara;
+//   let start = 1;
+//   if (currentPage > 1) {
+//     start = (currentPage - 1) * pagination;
+//   }
+//   let postIndex = await getListPage(
+//     `content/${blog_folder}/_index.md`,
+//     `http://adm.gempitamilenial.org/service/event-public?start=${start}&count=${pagination}`
+//   );
 
-  const mdxContent = await parseMDX(postIndex.content);
+//   const mdxContent = await parseMDX(postIndex.content);
 
-  return {
-    props: {
-      pagination: pagination,
-      currentPage: currentPage,
-      postIndex: postIndex,
-      mdxContent: mdxContent,
-    },
-    revalidate: 10,
-  };
-};
+//   return {
+//     props: {
+//       pagination: pagination,
+//       currentPage: currentPage,
+//       postIndex: postIndex,
+//       mdxContent: mdxContent,
+//     },
+//     revalidate: 10,
+//   };
+// };
