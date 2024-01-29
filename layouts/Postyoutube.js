e1cb854688d1aa43139450f62b7ba88236fb3eed
Emmanuel281 <47650883+Emmanuel281@users.noreply.github.com>
2024-01-29 11:35:05 +0700

    tambah google analytic & filter youtube

--- layouts/Postyoutube.js
+++ layouts/Postyoutube.js
@@ -7,73 +7,66 @@ const { blog_folder, pagination } = config.settingsyoutube;
 
 const PostYoutube = ({ title, post, posts, slug, currentPage, contentapi }) => {
   const totalPages = Math.ceil(posts.total_count / pagination);
-  const url = `${blog_folder}/${slug}`
-  // console.log("post");
-  // console.log(post);
-  // console.log("posts");
-  // console.log(posts);
-  // console.log("contentapi");
-  // console.log(contentapi);
+  const url = `${blog_folder}/${slug}`;
   return (
-    
-      <section className="section">
-        <div className="container">
-          {markdownify(
-            "Video Youtube Terbaru",
-            "h1",
-            "h1 text-center font-normal text-[56px]"
-          )}
-          <Postsyoutube posts={contentapi.data} slug={slug} type="youtube" />
-          <hr></hr>
-          <section>
-            <div className="container">
-              <div className="row">
-                <article className="col-12 text-center ">
-                  {post
-                    ? post.map((vids, index) => (
-                        <div key={index} className="col-12 mt-5">
-                          <div className="shadow">
-                            <div className="flex flex-col md:flex-row">
-                              <div className="w-270 flex-initial">
-                                <Image
-                                  className="max-w-full rounded-lg"
-                                  src={vids.img}
-                                  alt={""}
-                                  width={336}
-                                  height={142}
-                                  priority={true}
-                                />
-                              </div>
-                              <div className="flex-1 pl-2 text-left">
-                                <a
-                                  target="_blank"
-                                  href={vids.url}
-                                  rel="noopener noreferrer"
-                                >
-                                  <h4 className="h4 my-4">{vids.title}</h4>
-                                </a>
-                                <p>
-                                  {vids.description
-                                    .replace(/=/g, "")
-                                    .replace(/-/g, "")}
-                                </p>
-                              </div>
+    <section className="section">
+      <div className="container">
+        {markdownify(
+          "Video Youtube Terbaru",
+          "h1",
+          "h1 text-center font-normal text-[56px]"
+        )}
+        <Postsyoutube posts={contentapi.data} slug={slug} type="youtube" />
+        <hr></hr>
+        <section>
+          <div className="container">
+            <div className="row">
+              <article className="col-12 text-center ">
+                {post
+                  ? post.map((vids, index) => (
+                      <div key={index} className="col-12 mt-5">
+                        <div className="shadow">
+                          <div className="flex flex-col md:flex-row">
+                            <div className="w-270 flex-initial">
+                              <Image
+                                className="max-w-full rounded-lg"
+                                src={vids.img}
+                                alt={""}
+                                width={336}
+                                height={142}
+                                priority={true}
+                              />
+                            </div>
+                            <div className="flex-1 pl-2 text-left">
+                              <a
+                                target="_blank"
+                                href={vids.url}
+                                rel="noopener noreferrer"
+                              >
+                                <h4 className="h4 my-4">{vids.title}</h4>
+                              </a>
+                              <p>
+                                {vids.description
+                                  .replace(/=/g, "")
+                                  .replace(/-/g, "")}
+                              </p>
                             </div>
                           </div>
                         </div>
-                      ))
-                    : ""}
-                </article>
-              </div>
+                      </div>
+                    ))
+                  : ""}
+              </article>
             </div>
-          </section>
-          <Pagination
-            section={url}
-            totalPages={totalPages}
-            currentPage={currentPage}
-          />
-        </div>
-      </section>
+          </div>
+        </section>
+        <Pagination
+          section={url}
+          totalPages={totalPages}
+          currentPage={currentPage}
+        />
+      </div>
+    </section>
   );
 };
 
--- layouts/partials/Postsyoutube.js
+++ layouts/partials/Postsyoutube.js
@@ -1,34 +1,65 @@
 import Link from "next/link";
 
-const Postsyoutube = ({ posts, slug,  type }) => {
-// console.log(slug)
+const Postsyoutube = ({ posts, slug, type }) => {
+  // console.log(slug)
   return (
     <div className="section row pb-0">
       <div className="col-12">
         <div className="flex flex-nowrap overflow-x-auto px-4">
-          {posts.map((post, i) => (
-            post.id == slug ? (
-              <div key={`key-${i}`} className="m-1 min-w-fit">
-              <Link
-                className="btn btn-primary m-1 min-w-fit"
-                href={`/${type}/${post.id}`}
-                rel=""
-              >
-                {post.channel_name}
-              </Link>
-            </div>
+          {posts.map((post, i) =>
+            post.id == "b39a0496-a42f-4d5b-8712-b127fcdcee72" ? (
+              post.id == slug ? (
+                <div key={`key-${i}`} className="m-1 min-w-fit">
+                  <Link
+                    className="btn btn-primary m-1 min-w-fit"
+                    href={`/${type}/${post.id}`}
+                    rel=""
+                  >
+                    {post.channel_name}
+                  </Link>
+                </div>
+              ) : (
+                <div className="m-1 min-w-fit">
+                  <Link
+                    className="btn btn-outline-primary m-1 min-w-fit"
+                    href={`/${type}/${post.id}`}
+                    rel=""
+                  >
+                    {post.channel_name}
+                  </Link>
+                </div>
+              )
             ) : (
-              <div className="m-1 min-w-fit">
-              <Link
-                className="btn btn-outline-primary m-1 min-w-fit"
-                href={`/${type}/${post.id}`}
-                rel=""
-              >
-                {post.channel_name}
-              </Link>
-            </div>
+              ""
             )
-          ))}
+          )}
+          {posts.map((post, i) =>
+            post.id != "b39a0496-a42f-4d5b-8712-b127fcdcee72" ? (
+              post.id == slug ? (
+                <div key={`key-${i}`} className="m-1 min-w-fit">
+                  <Link
+                    className="btn btn-primary m-1 min-w-fit"
+                    href={`/${type}/${post.id}`}
+                    rel=""
+                  >
+                    {post.channel_name}
+                  </Link>
+                </div>
+              ) : (
+                <div className="m-1 min-w-fit">
+                  <Link
+                    className="btn btn-outline-primary m-1 min-w-fit"
+                    href={`/${type}/${post.id}`}
+                    rel=""
+                  >
+                    {post.channel_name}
+                  </Link>
+                </div>
+              )
+            ) : (
+              ""
+            )
+          )}
         </div>
       </div>
     </div>
